2fc3c7d2942ecfa5c43456857586206e9178a150
Dimitra Paranou <dimitra.paranou@student.uleige.com>
2019-03-23 17:01:46 +0200

    Fix grocery list show items

--- js/app.js
+++ js/app.js
@@ -1,32 +1,17 @@
 /*
 *
 */
-var app = angular.module("groceryApp", ["ngRoute", "groceryAppModule"]);
+var app = angular.module('groceryApp', []);
 
-app.controller("homeCtrl", function($scope) {
+app.controller("homeCtrl",["$scope", function($scope) {
   $scope.appTitle = "Grocery List";
-})
+}]);
 
-app.controller("grCtrl", function($scope) {
-  $scope.groceryList = [
-    {completed: true, itemName:'milk', date:'2019-03-20'},
-    {completed: true, itemName:'butter', date:'2019-03-17'},
-    {completed: true, itemName:'olive oil', date:'2019-03-21'},
-    {completed: true, itemName:'chocolate', date:'2019-03-15'}
-  ];
-})
-/*app.config(function($routeProvider) {
-    $routeProvider
-      .when("/", {
-        templateUrl: "views/first.html",
-        controller: "tutorialCtrl"
-      })
-      .when("/second", {
-        templateUrl:"views/second.html",
-        controller: "tutorialCtrl2"
-      })
-      .otherwise({
-        redirectTo:"/"
-      });
-});
-*/
+app.controller("groceryListCtrl", ["$scope", function($scope) {
+  $scope.groceryItems= [
+    {completed: true, itemName: "milk", date:'2019-03-20'},
+    {completed: true, itemName: "butter", date:'2019-03-17'},
+    {completed: true, itemName: "olive oil", date:'2019-03-21'},
+    {completed: true, itemName: "chocolate", date:'2019-03-15'}
+  ]
+}]);
