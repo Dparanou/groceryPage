545e1942e904d7e575979f3b0b8cdc559f4e22d5
Dimitra Paranou <dimitra.paranou@student.uleige.com>
2019-03-24 18:51:11 +0200

    Edit list and buttons for delete-edit-check

--- js/app.js
+++ js/app.js
@@ -3,36 +3,119 @@
 */
 var app = angular.module('groceryApp', ["ngRoute"]);
 
-app.config(function($routeProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
+  $locationProvider.hashPrefix('');
+
   $routeProvider
   .when("/", {
     templateUrl: "views/groceryList.html",
-    controller: "groceryListCtrl"
+    controller: "homeCtrl"
   })
   .when("/addItem",{
     templateUrl:"views/addItem.html",
     controller: "groceryListCtrl"
   })
-  .when("/addItem/:id",{
+  .when("/addItem/edit/:id/",{
     templateUrl:"views/addItem.html",
     controller: "groceryListCtrl"
   })
   .otherwise({
     redirectTo: "/"
   });
+}]);
+
+app.service("GroceryService", function() {
+  var groceryService = {};
+
+  groceryService.groceryItems= [
+    {id: 1, completed: false, itemName: "milk", date: new Date("April 2, 2019 11:12:00")},
+    {id: 2, completed: false, itemName: "butter", date: new Date("April 1, 2019 12:12:00")},
+    {id: 3, completed: false, itemName: "olive oil", date: new Date("April 2, 2019 20:12:00")},
+    {id: 4, completed: false, itemName: "chocolate", date: new Date("April 3, 2019 11:15:00")}
+  ];
+
+  groceryService.findById = function (id) {
+    for(var item in groceryService.groceryItems) {
+      if(groceryService.groceryItems[item].id === id) {
+        return groceryService.groceryItems[item];
+      }
+    }
+  };
+
+  groceryService.getNewId = function() {
+    if(groceryService.newId) {
+      groceryService.newId++;
+      return groceryService.newId;
+    }
+    else {
+      var maxId = _.max(groceryService.groceryItems, function(entry) {return entry.id; });
+      groceryService.newId = maxId.id + 1;
+      return groceryService.newId;
+    }
+  };
+
+  groceryService.save = function(entry) {
+
+    var updatedItem = groceryService.findById(entry.id);
+
+    if(updatedItem) {
+      updatedItem.completed = entry.completed;
+      updatedItem.itemName = entry.itemName;
+      updatedItem.date = entry.date;
+    }
+    else {
+      entry.id = groceryService.getNewId();
+      groceryService.groceryItems.push(entry);
+    }
+  };
+
+  groceryService.removeItem = function(entry) {
+    var index = groceryService.groceryItems.indexOf(entry);
+
+    groceryService.groceryItems.splice(index, 1);
+  };
+
+  groceryService.checked = function(entry) {
+    entry.completed = !entry.completed;
+  };
+
+  return groceryService;
 });
 
-app.controller("homeCtrl",["$scope", function($scope) {
-  $scope.appTitle = "Grocery List";
+app.controller("homeCtrl",["$scope", "GroceryService", function($scope, GroceryService) {
+
+  $scope.groceryItems = GroceryService.groceryItems;
+  console.log($scope.groceryItems);
+
+  $scope.removeItem = function(entry) {
+    GroceryService.removeItem(entry);
+  };
+
+  $scope.checked = function(entry) {
+    GroceryService.checked(entry);
+  };
+
 }]);
 
-app.controller("groceryListCtrl", ["$scope","$routeParams", function($scope, $routeParams) {
-  $scope.groceryItems= [
-    {completed: true, itemName: "milk", date:'2019-03-20'},
-    {completed: true, itemName: "butter", date:'2019-03-17'},
-    {completed: true, itemName: "olive oil", date:'2019-03-21'},
-    {completed: true, itemName: "chocolate", date:'2019-03-15'}
-  ]
+app.controller("groceryListCtrl", ["$scope","$routeParams", "$location", "GroceryService", function($scope, $routeParams, $location, GroceryService) {
 
-  $scope.rp = "Route parm " + $routeParams.id;
+  if(!$routeParams.id) {
+    $scope.groceryItem =  {id: 0, completed: false, itemName: "", date:new Date() };
+  }
+  else {
+      $scope.groceryItem =_.clone(GroceryService.findById(parseInt($routeParams.id)));
+  }
+
+  $scope.save = function() {
+    GroceryService.save( $scope.groceryItem );
+    $location.path("/");
+  };
+
 }]);
+
+app.directive("liGroceryItem", function() {
+  return {
+    restrict : "E",
+    templateUrl: "views/groceryItem.html"
+  };
+});
