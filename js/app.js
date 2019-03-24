470125b744c812d29efbbaded3f179e2610a76bd
Dimitra Paranou <dimitra.paranou@student.uleige.com>
2019-03-24 12:48:38 +0200

    Add views

--- js/app.js
+++ js/app.js
@@ -1,17 +1,38 @@
 /*
 *
 */
-var app = angular.module('groceryApp', []);
+var app = angular.module('groceryApp', ["ngRoute"]);
+
+app.config(function($routeProvider) {
+  $routeProvider
+  .when("/", {
+    templateUrl: "views/groceryList.html",
+    controller: "groceryListCtrl"
+  })
+  .when("/addItem",{
+    templateUrl:"views/addItem.html",
+    controller: "groceryListCtrl"
+  })
+  .when("/addItem/:id",{
+    templateUrl:"views/addItem.html",
+    controller: "groceryListCtrl"
+  })
+  .otherwise({
+    redirectTo: "/"
+  });
+});
 
 app.controller("homeCtrl",["$scope", function($scope) {
   $scope.appTitle = "Grocery List";
 }]);
 
-app.controller("groceryListCtrl", ["$scope", function($scope) {
+app.controller("groceryListCtrl", ["$scope","$routeParams", function($scope, $routeParams) {
   $scope.groceryItems= [
     {completed: true, itemName: "milk", date:'2019-03-20'},
     {completed: true, itemName: "butter", date:'2019-03-17'},
     {completed: true, itemName: "olive oil", date:'2019-03-21'},
     {completed: true, itemName: "chocolate", date:'2019-03-15'}
   ]
+
+  $scope.rp = "Route parm " + $routeParams.id;
 }]);
